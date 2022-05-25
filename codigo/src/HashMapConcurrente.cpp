dff205e0b622b451171a83e7a8ea3ed1baac0790
Your Name <you@example.com>
2022-05-25 15:44:12 -0300

    ejer 1 y 2 codigo

--- codigo/src/HashMapConcurrente.cpp
+++ codigo/src/HashMapConcurrente.cpp
@@ -14,20 +14,79 @@ HashMapConcurrente::HashMapConcurrente() {
     }
 }
 
+unsigned int getIndex(std::string clave){
+    return (int)clave[0] - 97;
+}
+
 unsigned int HashMapConcurrente::hashIndex(std::string clave) {
     return (unsigned int)(clave[0] - 'a');
 }
 
 void HashMapConcurrente::incrementar(std::string clave) {
-    // Completar (Ejercicio 2)
+    if (clave.length() < 1){
+        //No es un string valido
+        return;
+    }
+
+    int index = getIndex(clave);
+    if (index < 0 && index > 25){
+        //Si no es letra minuscula
+        return;
+    }
+
+    // FALTA
+    // Tenemos que hacer un array de mutex para que en cada letra tiramos lock y unlock
+    mtx[index].lock();
+
+    ListaAtomica<hashMapPair> l = _tabla[index];
+    hashMapPair par = std::make_pair(clave, 1);
+    if (l.longitud == 0){
+        l.insertar( par);
+    } else {
+        bool exit = false;
+        auto iter = l.begin();
+        while (iter != nullptr) {
+            if (iter*.first == clave) {
+                iter*.second += 1;
+                exit = true;
+            }
+            if (exit)
+                break;
+            iter++;
+        }
+        if (!exit) {
+            l.insertar(par);
+        }
+    }
+
+    mtx[index].unlock();
+    return;
+
 }
 
 std::vector<std::string> HashMapConcurrente::claves() {
-    // Completar (Ejercicio 2)
+    std::vector<std::string> res = new std::vector<std::string>();
+    for (unsigned int i = 0; i < HashMapConcurrente::cantLetras; i++) {
+        auto iter = tabla[i].begin();
+        while (iter != nullptr) {
+            res.push(iter*.first == clave);
+            iter++;
+        }
+    }
+    return res;
 }
 
 unsigned int HashMapConcurrente::valor(std::string clave) {
-    // Completar (Ejercicio 2)
+    int res = -1;
+    int index = getInted(clave);
+    auto iter = tabla[index].begin();
+    while (iter != nullptr) {
+        if (iter*.first == clave) {
+            return iter*.second;
+        }
+        iter++;
+    }
+    return 0;
 }
 
 hashMapPair HashMapConcurrente::maximo() {
